9f0530a575e5db288383860471f4d188e6eeaa2b
DNAWright <dfwrite@gmail.com>
2018-11-08 18:11:54 -0500

    Forgot to add writeFile Method

--- src/FastaParser.java
+++ src/FastaParser.java
@@ -8,14 +8,21 @@ public class FastaParser
 
     public static void main(String[] args) throws Exception
     {
-        List<FastaSequence> fastaList = FastaSequence.readFastaFile("/Users/danielwright/JavaProjects/FastaParser/src/SampleSequence.txt");
 
-        for(FastaSequence fs : fastaList)
+        File try2 = new File("/Users/danielwright/JavaProjects/FastaParser/src/SampleSequence2.txt");
+        File out2 = new File("TrialOutput.txt");
+        // List<FastaSequence> fastaList = FastaSequence.readFastaFile("/Users/danielwright/JavaProjects/FastaParser/src/SampleSequence2.txt");
+
+        FastaSequence hi2 = new FastaSequence();
+
+        hi2.writeUnique(try2, out2);
+
+        /** for(FastaSequence fs : fastaList)
         {
             System.out.println(fs.getHeader());
             System.out.println(fs.getSequence());
             System.out.println(fs.getGCRatio());
-        }
+        } **/
 
     }
 
--- src/FastaSequence.java
+++ src/FastaSequence.java
@@ -1,8 +1,11 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.util.List;
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+import static java.util.Map.Entry.*;
+
 
 public class FastaSequence
 {
@@ -66,4 +69,53 @@ public class FastaSequence
         fReader.close();
         return sequenceList;
     }
+
+    public static void writeUnique(File inFile, File outFile ) throws Exception
+    {
+        BufferedReader fReader = new BufferedReader(new FileReader(inFile));
+        BufferedWriter fastaWriter = new BufferedWriter(new FileWriter(outFile));
+        Map<String,Integer> seqMap = new LinkedHashMap<>();
+        String line = fReader.readLine();
+
+        while(line != null)
+        {
+            StringBuilder build = new StringBuilder();
+            if(line.startsWith(">"))
+            {
+                line = fReader.readLine();
+            }
+
+            while(line != null && ! line.startsWith(">"))
+            {
+                build.append(line);
+                line = fReader.readLine();
+            }
+
+            String seqKey = build.toString().trim();
+            if(!seqMap.containsKey(seqKey))
+            {
+                seqMap.put(seqKey, 1);
+            }
+            else
+                seqMap.replace(seqKey, seqMap.get(seqKey)+1);
+
+        }
+
+        seqMap.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (x,y)-> {throw new AssertionError();},
+                        LinkedHashMap::new
+                ));
+
+
+        for(Map.Entry<String, Integer> entry : seqMap.entrySet()) {
+        System.out.println(entry.getKey()+" : "+entry.getValue());
+    }
+
+    }
+
 }
